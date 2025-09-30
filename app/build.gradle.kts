d56c2d37f4837717d7166d762232456af4d13606
takusan23 <takumu1200@gmail.com>
2025-09-30 19:16:58 +0900

    ライブラリアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "io.github.takusan23.ultrahdrbluredit"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.takusan23.ultrahdrbluredit"
         minSdk = 34
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
